f19a5d9e5a8bfd75dd355acd712a2261a892a09b
CrazyCreep <390997723@qq.com>
2019-10-18 23:04:29 +0800

    迁移Androidx

---
+++ app/src/main/java/com/example/weatherbyty/db/Province.java
@@ -0,0 +1,33 @@
+package com.example.weatherbyty.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Province extends LitePalSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public int setProvinceCode(){
+        return provinceCode;
+    }
+
+    public void getProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
